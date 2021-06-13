629b92918fb23d9a43351020d274823586647e49
leviathan <22543899@sun.ac.za>
2021-06-14 00:07:01 +0200

    Fixed missing js script

--- Propinquity/src/app/app.module.ts
+++ Propinquity/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GroupsComponent } from './groups/groups.component';
@@ -10,6 +11,38 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AddGroupComponent } from './add-group/add-group.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GroupFormComponent } from './group-form/group-form.component';
+
+import { MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule, MatFormFieldModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -24,7 +57,10 @@ import { GroupFormComponent } from './group-form/group-form.component';
     FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CommonModule
+    CommonModule,
+    FormsModule, 
+    MatDialogModule,
+    MatFormFieldModule 
   ],
   providers: [],
   bootstrap: [AppComponent]

c54e3fd24b116a53ae14cd32d5d1343528079494
leviathan <22543899@sun.ac.za>
2021-06-14 00:41:49 +0200

    Fixes

--- Propinquity/src/app/app.module.ts
+++ Propinquity/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GroupsComponent } from './groups/groups.component';
@@ -12,37 +11,6 @@ import { AddGroupComponent } from './add-group/add-group.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GroupFormComponent } from './group-form/group-form.component';
 
-import { MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatStepperModule, MatFormFieldModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -58,9 +26,6 @@ import { MatAutocompleteModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     CommonModule,
-    FormsModule, 
-    MatDialogModule,
-    MatFormFieldModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
